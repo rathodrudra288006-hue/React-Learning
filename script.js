a49e56e2d1379a3c91f522e789e86e143432632d
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-05-26 17:49:01 +0530

    today last change in code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// Day 1 of react
+
+const heading = React.createElement("h1", {}, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
