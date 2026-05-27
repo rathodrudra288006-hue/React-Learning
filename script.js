3b917b808bf0086de597b42615efd3fad4d8d295
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-05-27 16:31:28 +0530

    for 2 chid id of first div

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ root.render(heading);
 // Day 2
 /**
  * <div  id="parent">
-  <div id="chid">
+  <div id="chid1">
+    <h1>This is H1 tag</h1> 
+    <h2>This is h2 tag</h2>
+  </div>
+  <div id="chid2">
     <h1>This is H1 tag</h1> 
     <h2>This is h2 tag</h2>
   </div>
@@ -20,13 +24,15 @@ root.render(heading);
 ReactElement(Object) => HTML (browser understand)
 
  */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "chid" }, [
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "chid1" }, [
     React.createElement("h1", {}, "This is H1 tag"),
     React.createElement("h2", {}, "This is H2 tag"),
   ]),
-);
+  React.createElement("div", { id: "chid2" }, [
+    React.createElement("h1", {}, "This is H1 tag"),
+    React.createElement("h2", {}, "This is H2 tag"),
+  ]),
+]);
 const p1 = ReactDOM.createRoot(document.getElementById("p1"));
 p1.render(parent);
