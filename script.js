f9f3f7029c4048c4aaf90fc2b60900ff4ce39084
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-05-27 16:24:39 +0530

    nested div in react

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Day 1 of react
 
-const heading = React.createElement("h1", {}, "Hello World");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World",
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+// Day 2
+/**
+ * <div  id="parent">
+  <div id="chid">
+    <h1>This is H1 tag</h1> 
+  </div>
+</div>
+
+ReactElement(Object) => HTML (browser understand)
+
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "chid" },
+    React.createElement("h1", {}, "This is H1 tag"),
+  ),
+);
+const p1 = ReactDOM.createRoot(document.getElementById("p1"));
+p1.render(parent);
