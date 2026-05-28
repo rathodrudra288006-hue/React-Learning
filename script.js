09fe6c905f5d186d9b59378875781b5bf75d0074
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-05-28 17:31:21 +0530

    Change div ID from 'chid' to 'chid-2'

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h1", {}, "This is H1 tag"),
     React.createElement("h2", {}, "This is H2 tag"),
   ]),
-  React.createElement("div", { id: "chid" }, [
+  React.createElement("div", { id: "chid-2" }, [
     React.createElement("h1", {}, "This is H1 tag"),
     React.createElement("h2", {}, "This is H2 tag"),
   ]),
