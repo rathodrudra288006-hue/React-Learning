c5840214f5522b7823cc1ab17e53048942e33d97
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-06-12 17:42:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#namste-react course
+
+#parcel
+-dev-build
+-local server
+-HMR:hot module replacement
+-file watching algorithum = written in c++
+-caching = faster builds
+-image optimization
+-minification
+-bundling
+-compress
+-consistant hashing
+-code spliting
+-differential bundling = sporting older browser
+-diagnostic
+-error handling
+-https
+-tree shanking algorithm = remove unused code
+-different dev and prod bundles
---
+++ script.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+/** 
+  =>Header
+  -Logo
+  -Nav Item 
+  =>Body
+  -Search
+  -RestarantContainer
+    -RestaruntCard
+      -img
+      -name of the res ,star rating,cuising,delivery time,etc
+  =>Footer
+  -CopyRight
+  -Link
+  -Address
+  -Content
+*/
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Logo-container">
+        <img
+          className="Logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contect Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaruntCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        alt="res-img"
+        src="https://cdn3.foodviva.com/static-content/food-images/rice-recipes/vegetable-pulav-recipe/vegetable-pulav-recipe.jpg"
+      />
+      <h3>Meghana Food</h3>
+      <h4>Biryani,North India,Asian</h4>
+      <h4>4.4 Star</h4>
+      <h4>38 min</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaruntCard />
+        <RestaruntCard />
+        <RestaruntCard />
+        <RestaruntCard />
+        <RestaruntCard />
+        <RestaruntCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
