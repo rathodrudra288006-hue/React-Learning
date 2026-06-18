ccf5e615a70742e56105d343ec468850f044f372
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-06-18 17:46:42 +0530

    Update Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Logo-container">
+        <img
+          className="Logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contect Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
