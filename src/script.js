5cd33d0b2025379a52aeb39da8b56f41764e4709
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-06-18 17:45:30 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,26 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <div className="Logo-container">
-        <img
-          className="Logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
-        />
-      </div>
-      <div className="nav-item">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contect Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+
 const RestaruntCard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, avgRating, costForTwo, cuisines } =
