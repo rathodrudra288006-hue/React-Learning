730c27c1ed12bc98c96f7b8d752c43adb2c023b0
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-06-24 14:48:44 +0530

    Update shimmer.js

--- src/Components/shimmer.js
+++ src/Components/shimmer.js
@@ -10,6 +10,12 @@ const Shimmer = () => {
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
     </div>
   );
 };
