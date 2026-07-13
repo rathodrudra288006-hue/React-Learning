bbffca9026364069729d85521c46bc5299ecb4f7
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-07-13 21:14:42 +0530

    Remove fetchMenu function from RestaurantMenu
    Removed the fetchMenu function from RestaurantMenu component.

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,15 +1,5 @@
 
 const RestaurntMenu = () => {
-
-  const fetchMenu = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5459818&lng=72.9292495&restaurantId=340376&catalog_qa=undefined&submitAction=ENTER",
-    );
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json.data);
-  };
-
   return resInfo === null ? (
     <Shimmer />
   ) : (
