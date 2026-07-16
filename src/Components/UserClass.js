c46a31054c6ce68584f1b283e6cfa3b8d218c285
rathodrudra288006-hue <rathodrudra288006@gmail.com>
2026-07-16 21:45:39 +0530

    Update UserClass.js

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -3,7 +3,7 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    
     console.log("Child Constructor");
   }
 
@@ -13,22 +13,12 @@ class UserClass extends React.Component {
 
   render() {
     const { name } = this.props;
-    const { count } = this.state;
+   
     console.log("Child Render");
 
     return (
       <div className="user-card">
-        <h1>Count:{this.state.count} </h1>
-        <button
-          onClick={() => {
-            // Never Update State Variable Directly
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        
         <h2>Name:{this.props.name}</h2>
         <h3>Location:Ahmedabad</h3>
         <h4>Contect:rathod*****2****6@gmail.com</h4>
