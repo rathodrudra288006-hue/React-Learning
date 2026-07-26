495130c4947e6d66686d5ec191a3be436f3ae2b7
Rudra Rathod <rathodrudra288006@gmail.com>
2026-07-26 15:55:08 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
         <img className="w-56" src={LOGO_URL} />
       </div>
       <div className="nav-item">
-        <ul className="flex">
+        <ul className="flex p-4 m-4">
           <li>Online Status:{onlineStatus ? "✅" : "⭕"}</li>
           <li>
             <Link to="/">Home</Link>
